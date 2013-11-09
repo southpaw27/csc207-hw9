b37519b38234b8906ad1334df1db072a3002a118
m13hall <hallmira@grinnell.edu>
2013-11-09 16:41:19 -0600

    added partial rr for f5. I got stuck on how big we can assume c is-- negligible or no?

--- src/RecurrenceRelations.java
+++ src/RecurrenceRelations.java
@@ -13,6 +13,34 @@ public class RecurrenceRelations {
      * f6(n) = 2^n + c(2^n - 1)
      * 
      */
+    
+    /*
+     * f5(n) = c + 2(f5(n/2))
+     * 
+     * f5(1) = 1
+     * 
+     * f5(n) = c + 2(f5(n/2))
+     * f5(n) = c + 2(c + 2(f5(n/4))) = 3c + 4(f5(n/8))
+     * f5(n) = 3c + 4(c + 2(f5(n/16))) = 7c + 8(f5(n/32))
+     * 
+     * f5(n) = (2^k - 1)c + 2^k * ( f5(n/(2^k+1)))
+     * 
+     * if n = 2^k+1
+     * then
+     * f5(n) = (2^k - 1)c + 2^k *(f5(n/n)) = (2^k - 1)c + 2^k(f5(1))
+     * f5(n) = (2^k - 1)c + 2^k
+     *
+     * furthermore, n * 2^-1 = 2^k+1 * 2^-1
+     * n/2 = 2^k
+     * 
+     * so
+     * f5(n) = (n/2 - 1) c + n/2
+     * f5(n) = c * n/2 - c + n/2
+     * f5(n) = (c + 1) n/2 - c
+     * 
+     * can we assume the n's dominate???? how big is c???
+     * 
+     */
     public RecurrenceRelations() {
 	// TODO Auto-generated constructor stub
     }
