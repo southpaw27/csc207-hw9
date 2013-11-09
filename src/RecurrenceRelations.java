b8d8ac502ba1079300781e79efcfd8bedbdb4c13
m13hall <hallmira@grinnell.edu>
2013-11-09 16:09:18 -0600

    added recurrence relation for f6

---
+++ src/RecurrenceRelations.java
@@ -0,0 +1,20 @@
+
+public class RecurrenceRelations {
+
+    /*
+     * f6(n) = c + 2 * f6(n-1)
+     * 
+     * f6(1) = 1
+     * f6(2) = c + 2*(1) = c + 2
+     * f6(3) = c + 2*(c + 2) = 3c + 4
+     * f6(4) = c + 2*(3c + 4) = 7c + 8
+     * f6(5) = c + 2(7c + 8) = 15c + 16
+     * 
+     * f6(n) = 2^n + c(2^n - 1)
+     * 
+     */
+    public RecurrenceRelations() {
+	// TODO Auto-generated constructor stub
+    }
+
+}
